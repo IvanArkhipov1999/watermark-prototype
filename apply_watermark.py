4776fc15af1968cdbd0a6e942b7528fd93dc375c
ivan <arkhipov.iv99@mail.ru>
2023-03-01 20:16:16 +0300

    simple watermark insert

--- apply_watermark.py
+++ apply_watermark.py
@@ -5,3 +5,4 @@ def apply_watermark(path_to_file):
 	compile_to_assembler(path_to_file)
 	insert_commands("./test.s")
 	compile_to_binary("./modified.s");
+	insert_watermark("./modified")
--- insert.py
+++ insert.py
@@ -10,6 +10,31 @@ def insert_commands(path_to_file):
 	contents.insert(index + 4, "\tnop\n");
 	contents.insert(index + 5, ".L2:\n");
 
-	with open("modified.s", "w") as f:
+	with open(path_to_file, "w") as f:
 		contents = "".join(contents)
 		f.write(contents)
+
+def insert_watermark(path_to_file):
+	with open(path_to_file, "rb") as f:
+		file_bytes = bytearray(f.read())
+	index = 0
+
+	for byte in file_bytes:
+		if byte == 0x90:
+			is_found = True
+			for i in range(4):
+				if file_bytes[index + i] != 0x90:
+					is_found = False
+			if is_found:
+				break
+		index = index + 1
+	print(hex(index))
+		
+	file_bytes[index] = 0x77
+	file_bytes[index + 1] = 0x6f
+	file_bytes[index + 2] = 0x72
+	file_bytes[index + 3] = 0x64
+
+	with open("watermarked", "wb") as f:
+		f.write(file_bytes)
+
