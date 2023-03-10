814c5bd5732007de46f4c7ba1e6f94cce45ff2f8
ivan <arkhipov.iv99@mail.ru>
2023-03-10 17:47:07 +0300

    compile_file is ready

--- apply_watermark.py
+++ apply_watermark.py
@@ -2,8 +2,8 @@ from compile import *
 from insert import *
 
 def apply_watermark(source_file):
-	compile_to_assembler(source_file, "origin.s", "gcc", [])
+	compile_file(source_file, "origin.s", "gcc", ["-S"])
 	# TODO: count number of commands
 	insert_nop_commands("origin.s", 8, "inserted_commands.s")
-	#compile_to_binary("./modified.s");
+	compile_file("inserted_commands.s", "inserted_commands", "gcc", []);
 	#insert_watermark("./modified")
--- compile.py
+++ compile.py
@@ -1,17 +1,17 @@
 import os
 
 # -----------------------------------------------------------
-# Compiles given source file to assembler file with given name by 
+# Compiles given source file to output file with given name by 
 # given compiler and flags.
 #
 # Parameters:
 #
 # source_file: path to source file for compiling
-# assembler_file: path to assembler file, all directories in path should exist
+# assembler_file: path to output file, all directories in path should exist
 # compiler: compiler for compiling
 # flags: list of flags for compiling
 # -----------------------------------------------------------
-def compile_to_assembler(source_file, assembler_file, compiler, flags):
+def compile_file(source_file, output_file, compiler, flags):
 	# Setting compiler
 	compiling_command = compiler + " "
 	# Setting flags
@@ -20,12 +20,6 @@ def compile_to_assembler(source_file, assembler_file, compiler, flags):
 	# Setting path to source file
 	compiling_command = compiling_command + source_file
 	# Setting path for assembler file
-	compiling_command = compiling_command + " -S -o " + assembler_file
+	compiling_command = compiling_command + " -o " + output_file
 	# Executing command
-	os.system(compiling_command)
-
-def compile_to_binary(path_to_file):
-	compiling_command = "gcc "
-	compiling_command = compiling_command + path_to_file
-	compiling_command = compiling_command + " -o modified"
 	os.system(compiling_command)
