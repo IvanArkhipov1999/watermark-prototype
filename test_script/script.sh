e017fe5761cc195e1a971197e088bd9235804678
ivan <arkhipov.iv99@mail.ru>
2023-03-20 15:20:48 +0300

    add echo for debug

--- test_script/script.sh
+++ test_script/script.sh
@@ -4,6 +4,7 @@ mkdir tmp
 cd tmp
 python3 ../main.py apply_watermark ../tests/test.cpp "Test message" gcc
 output=$(python3 ../main.py extract_watermark watermarked)
+echo $output
 failure=1
 if [[ "$output" == "Test message" ]]; then
   failure=0
