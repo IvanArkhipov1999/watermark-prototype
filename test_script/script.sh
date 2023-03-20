bd2ec243371265cadcbd39bffec32e7d2b7472a3
ivan <arkhipov.iv99@mail.ru>
2023-03-20 15:10:14 +0300

    add testing script

---
+++ test_script/script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+mkdir tmp
+cd tmp
+python3 ../main.py apply_watermark ../tests/test.cpp "Test message" gcc
+output=$(python3 ../main.py extract_watermark watermarked)
+failure=1
+if [[ "$output" == "Test message" ]]; then
+  failure=0
+fi
+cd ..
+rm -r tmp
+rm -r __pycache__
+exit $failure
